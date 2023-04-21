47e94c5b19c37549605ffadc92ab67a9ca3e1ae9
huang-hong <huang.hong@navercorp.com>
2023-04-21 18:44:39 +0800

    add category selection

--- root/build-time-data/documents.data.ts
+++ root/build-time-data/documents.data.ts
@@ -2,13 +2,30 @@ import { createContentLoader } from 'vitepress';
 
 export default {
   async load() {
-    const posts = await createContentLoader(
-      'documents/*.md' /* options */,
-    ).load();
+    const posts = await createContentLoader('documents/*.md' /* options */, {
+      excerpt: true,
+      transform(rawData) {
+        return rawData
+          .sort((a, b) => {
+            return (
+              +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date)
+            );
+          })
+          .map((page) => {
+            return {
+              author: page.frontmatter.author,
+              updateTime: page.frontmatter.date,
+            };
+          });
+      },
+    }).load();
 
     const catalogueWithTime = {};
     const catalogueWithAuthor = {};
-    posts.forEach((post) => {});
+    posts.forEach((post) => {
+      const { author, updateTime } = post;
+      console.log(post);
+    });
 
     return {
       catalogueWithTime,
--- root/custom-pages/components/catalogueIndex.vue
+++ root/custom-pages/components/catalogueIndex.vue
@@ -10,7 +10,7 @@ enum CATALOGUE_TYPE {
 }
 let catalogueType = ref(CATALOGUE_TYPE.BY_TIME);
 
-const postsList = computed(async () => {
+const postsList = computed(() => {
   const { catalogueWithTime = {}, catalogueWithAuthor = {} } = data;
 
   return catalogueType.value === CATALOGUE_TYPE.BY_TIME
@@ -18,22 +18,32 @@ const postsList = computed(async () => {
     : catalogueWithAuthor;
 });
 
-function setCatalogue(e) {
-  catalogueType.value = e.value;
+function setCatalogue(value) {
+  catalogueType.value = value;
 }
 </script>
 
 <template>
   <div :class="$style.buttons">
-    <div>按时间归档</div>
-    <div>按作者归档</div>
+    <div
+      @click="setCatalogue(CATALOGUE_TYPE.BY_TIME)"
+      :class="{ [$style.active]: catalogueType === CATALOGUE_TYPE.BY_TIME }"
+    >
+      按时间归档
+    </div>
+    <div
+      @click="setCatalogue(CATALOGUE_TYPE.BY_AUTHOR)"
+      :class="{ [$style.active]: catalogueType === CATALOGUE_TYPE.BY_AUTHOR }"
+    >
+      按作者归档
+    </div>
   </div>
   <div>
     <div v-for="(catalogue, index) in Object.keys(postsList)" :key="index">
       <h1>{{ catalogue }}</h1>
       <ul>
         <li
-          v-for="(document, documentIndex) in postsList(catalogue)"
+          v-for="(document, documentIndex) in postsList[catalogue]"
           :key="`${index}-${documentIndex}`"
         >
           <a :href="document.url">{{ document.title }}</a>
@@ -46,5 +56,23 @@ function setCatalogue(e) {
 <style lang="less" module>
 .buttons {
   display: flex;
+  div {
+    border: solid 2px var(--vp-c-text-1);
+    padding: 4px 10px;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: all 0.25s;
+    &:not(:last-child) {
+      margin-right: 8px;
+    }
+    &.active {
+      color: var(--vp-c-brand);
+      border-color: var(--vp-c-brand);
+    }
+    &:hover {
+      color: var(--vp-c-brand);
+      border-color: var(--vp-c-brand);
+    }
+  }
 }
 </style>
